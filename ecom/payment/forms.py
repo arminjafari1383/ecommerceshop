2238407da915ab96712fb4b3d5019c1bb0803602
armin <arminjafri138386@gmail.com>
2024-07-06 15:57:24 +0330

    compelte info billing

--- ecom/payment/forms.py
+++ ecom/payment/forms.py
@@ -16,3 +16,15 @@ class ShippingForm(forms.ModelForm):
         model = ShippingAddress
         fields = ['shipping_full_name','shipping_email','shipping_address1','shipping_address2','shipping_state','shipping_city','shipping_zipcode','shipping_country']
         exclude = ['user',]
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Name on card'}),required=True)
+    card_number = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Card Number'}),required=True)
+    card_exp_date = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Expiration Date'}),required=True) 
+    card_cvv_number = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'CVV Code'}),required=True)
+    card_address1 = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Biliing address1'}),required=True)
+    card_address2 = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Billin address 2'}),required=False)
+    card_city = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Billing City'}),required=True)
+    card_state = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Billing State'}),required=True)
+    card_zipcode = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Billing zipcode'}),required=True)
+    card_country =forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-contorol','placeholder':'Billing country'}),required=True)
--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import *
 from payment.models import ShippingAddress
 from django.shortcuts import  get_object_or_404
 from django.contrib import messages
@@ -11,6 +11,13 @@ def billing_info(request):
         #GET the cart
         cart = Cart(request)
         cart_products = cart.get_prods
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, "billing_info.html",{"cart_products":cart_products,"ShippingForm":ShippingForm,"billing_form":billing_form})
+        else:
+            billing_form = PaymentForm()
+            return render(request, "billing_info.html",{"cart_products":cart_products,"ShippingForm":ShippingForm,"billing_form":billing_form})
+
         shipping_form = request.POST
         return render(request, "billing_info.html",{"cart_products":cart_products,"shipping_form":shipping_form})
 
