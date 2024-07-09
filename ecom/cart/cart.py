ab64bb790977b5989934d4ee92098b405d48a43d
armin <arminjafri138386@gmail.com>
2024-07-09 10:06:22 +0330

    order items inline

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,116 +1,96 @@
-# have an error when type int have an error 
-from store.models import Product,Profile
+from store.models import Product, Profile
 
 class Cart():
-    def __init__(self,request):
+    def __init__(self, request):
         self.session = request.session
-        #GET request
         self.request = request
-        #Get the current session key if it exists
         cart = self.session.get('session_key')
 
-        #if the user is new, no session key! create one!
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
-        
-        #Make sure cart is avaliable on all pages of site
+
         self.cart = cart
-    def db_add(self,product, quantity):
+
+    def cart_summary(self, request):
+        cart = Cart(request)
+        cart_products = cart.get_prods()
+        totals = cart.cart_total()
+        return render(request, "cart_summary.html", {"cart_products": cart_products, "totals": totals})
+
+    def db_add(self, product, quantity):
         product_id = str(product)
         product_qty = str(quantity)
-        #logic
         if product_id in self.cart:
             pass
         else:
-            # self.cart[product_id] = {'price' : str(product.price)}
             self.cart[product_id] = str(product_qty)
-        self.session.modified = True 
+        self.session.modified = True
 
-        #Deal with logged in user
         if self.request.user.is_authenticated:
-            #GET THE CURRENT USER PROFILE
-            current_user = Profile.objects.filter(user__id = self.request.user.id)
-            carty = str(self.cart)
-            carty = carty.replace("\'","\"")
-            #save carty to the profile model
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            carty = str(self.cart).replace("'", "\"")
             current_user.update(old_cart=str(carty))
+
     def add(self, product, quantity):
         product_id = str(product.id)
         product_qty = str(quantity)
-        #logic
         if product_id in self.cart:
             pass
         else:
-            # self.cart[product_id] = {'price' : str(product.price)}
             self.cart[product_id] = str(product_qty)
-        self.session.modified = True 
+        self.session.modified = True
 
-        #Deal with logged in user
         if self.request.user.is_authenticated:
-            #GET THE CURRENT USER PROFILE
-            current_user = Profile.objects.filter(user__id = self.request.user.id)
-            carty = str(self.cart)
-            carty = carty.replace("\'","\"")
-            #save carty to the profile model
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            carty = str(self.cart).replace("'", "\"")
             current_user.update(old_cart=str(carty))
 
     def __len__(self):
         return len(self.cart)
+
     def get_prods(self):
-        #GET ids from cart
         product_ids = self.cart.keys()
-        #Use ids to lookup products in database model
-        products = Product.objects.filter(id__in = product_ids)
-        #RETURN those looked up
+        products = Product.objects.filter(id__in=product_ids)
         return products
+
     def get_quants(self):
         quantities = self.cart
         return quantities
-    def update(self,product,quantity):
+
+    def update(self, product, quantity):
         product_id = str(product)
-        product_qty = int(product.quantity)
-
-        #Get cart 
+        product_qty = int(quantity)
         ourcart = self.cart
-        #Update Dictionary/cart
         ourcart[product_id] = product_qty
-
         self.session.modified = True
-        thing = self.cart
-        return thing
+        return ourcart
+
     def delete(self, product):
         product_id = str(product)
-        # Delete from dicitionary/cart
         if product_id in self.cart:
             del self.cart[product_id]
         self.session.modified = True
+
         if self.request.user.is_authenticated:
-            #GET THE CURRENT USER PROFILE
-            current_user = Profile.objects.filter(user__id = self.request.user.id)
-            carty = str(self.cart)
-            carty = carty.replace("\'","\"")
-            #save carty to the profile model
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            carty = str(self.cart).replace("'", "\"")
             current_user.update(old_cart=str(carty))
+
     def cart_total(self):
-    # Get product ids
         product_ids = self.cart.keys()
-    # Lookup those keys in our products database model
         products = Product.objects.filter(id__in=product_ids)
         quantities = self.cart
-    # Start counting at 0
         total = 0
         for key, value in quantities.items():
-        # Convert key string into integer so we can do math
             key = int(key)
             try:
-                value = int(value)  # Ensure quantity is an integer
+                value = int(value)
             except ValueError:
-                continue  # Skip this item if it can't be converted to an integer
+                continue
             for product in products:
                 if product.id == key:
                     if product.is_sale:
-                        total = total + (product.sale_price * value)
+                        total += product.sale_price * value
                     else:
-                        total = total + (product.price * value)
-            return total
-  
+                        total += product.price * value
+        return total
--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import *
-
+from django.contrib.auth.models import User
 
 #Register the model on the admin section thing
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 
+# create an OrderItem Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+#Extend our order model 
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    # readonly_fields = ["date_ordered"]
+    inlines = [OrderItemInline]
+
+
+#unregister order model
+admin.site.unregister(Order)
+
+
+# RE-register our order and orderitems
+admin.site.register(Order,OrderAdmin)
